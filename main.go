b58e0cc5584db6f7060ee659c0e90cfa93117c8d
MrShanks <simonestaffoli@gmail.com>
2025-11-27 11:42:22 +0100

    feat: add templates

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
+var tpl = template.Must(template.ParseGlob("templates/*.html"))
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if err := tpl.Execute(w, "home.html"); err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", HomeHandler)
 
 	log.Println("Server running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
